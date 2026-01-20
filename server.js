eddac861f53a50da3a0ca371aa869cf79ead1f08
yuviduyasasvin <yasasvinyuvidu@gmail.com>
2026-01-20 11:40:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin: "http://localhost:5173", // your frontend URL
+        origin: ["http://localhost:5173"], // your frontend URL
         methods: ["GET","POST"],
         credentials: true
     }
