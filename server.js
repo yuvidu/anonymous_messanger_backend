5c9d680ce514d82f2744a0b5c0cfcbda529fd9f6
yuviduyasasvin <yasasvinyuvidu@gmail.com>
2026-01-23 12:02:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin: ["http://localhost:5173","https://anonymous-yuvidu-frontend.netlify.app"], // your frontend URL
+        origin: ["http://localhost:5173","https://anonymous-yuvidu-frontend.netlify.app","https://sex-cat-yuvidu.netlify.app"], // your frontend URL
         methods: ["GET","POST"],
         credentials: true
     }
@@ -132,7 +132,7 @@ io.on("connection",(socket)=>{
 
 
 app.use(cors({ 
-    origin: ["http://localhost:5173", "https://anonymous-yuvidu-frontend.netlify.app"], // your frontend URLs
+    origin: ["http://localhost:5173", "https://anonymous-yuvidu-frontend.netlify.app","https://sex-cat-yuvidu.netlify.app"], // your frontend URLs
     credentials: true}
 ));
 
